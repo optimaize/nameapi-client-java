bb2f3b7d36a972d9c42cfa1c3b9bb2ccf5da21ce
Fabian Kessler <fabian.kessler@optimaize.com>
2014-04-15 10:32:11 +0200

    Providing NameApiModeFactory for the basic mode.

---
+++ src/main/java/org/nameapi/client/lib/NameApiModeFactory.java
@@ -0,0 +1,39 @@
+package org.nameapi.client.lib;
+
+import com.optimaize.command4j.Mode;
+import com.optimaize.command4j.ext.extensions.exception.exceptiontranslation.CombinedExceptionTranslator;
+import com.optimaize.command4j.ext.extensions.exception.exceptiontranslation.ExceptionTranslationExtension;
+import com.optimaize.soapworks.client.Keys;
+import com.optimaize.soapworks.client.exensions.exceptiontranslation.DefaultClientExceptionTranslator;
+import com.optimaize.soapworks.client.exensions.exceptiontranslation.SoapFaultExceptionTranslator;
+import com.optimaize.soapworks.common.host.Host;
+import org.jetbrains.annotations.NotNull;
+import org.nameapi.client.commonwsdl.Conversions;
+import org.nameapi.ontology.input.context.Context;
+
+/**
+ * Provides Mode instances that contain the minimum.
+ *
+ * @author Fabian Kessler
+ */
+public class NameApiModeFactory {
+
+    private static final Mode minimalMode = Mode.create()
+            .with(Keys.PORT_URL_FACTORY, new NameApiPortUrlFactory())
+            .with(ExceptionTranslationExtension.TRANSLATOR, new CombinedExceptionTranslator(new DefaultClientExceptionTranslator(), new SoapFaultExceptionTranslator()))
+            .with(Keys.HOST, new Host("api.nameapi.org", 80))
+    ;
+
+    /**
+     * You can take this and extend for your setup if you need more.
+     *
+     * <p>Example: .with(StdoutLoggingExtension.enabled())</p>
+     *
+     * <p>If the minimal default is not exactly what you need, for example because you were given a different
+     * host name, then create your own from scratch.</p>
+     */
+    @NotNull
+    public static Mode minimal(Context context) {
+        return minimalMode.with(NameApiKeys.CONTEXT, Conversions.convert(context));
+    }
+}
---
+++ src/test/functional/java/org/nameapi/client/services/FunctionalTestsNameApiModeFactory.java
@@ -0,0 +1,38 @@
+package org.nameapi.client.services;
+
+import com.optimaize.command4j.Mode;
+import com.optimaize.command4j.ext.extensions.logging.stdoutlogging.StdoutLoggingExtension;
+import org.jetbrains.annotations.NotNull;
+import org.nameapi.client.lib.NameApiModeFactory;
+import org.nameapi.ontology.input.context.Context;
+import org.nameapi.ontology.input.context.ContextBuilder;
+import org.nameapi.ontology.input.context.Priority;
+
+/**
+ * @author Fabian Kessler
+ */
+public class FunctionalTestsNameApiModeFactory extends NameApiModeFactory {
+
+    private static final String API_KEY = "fcb754ee9a95b2863d0f969936b87166-unitTests"; //null;
+
+    private static final Mode unitTestMode = minimal(makeContext())
+            //.with(TimeoutExtension.TIMEOUT, Duration.millis(5000))
+            .with(StdoutLoggingExtension.enabled())
+    ;
+
+    private static Context makeContext() {
+        if (API_KEY==null) {
+            throw new RuntimeException("Set the api key variable to run the functional tests (get it from nameapi.org)!");
+        }
+        return new ContextBuilder()
+                .apiKey(API_KEY)
+                .priority(Priority.REALTIME)
+        .build();
+    }
+
+    @NotNull
+    public static Mode functionalTest() {
+        return unitTestMode;
+    }
+
+}
