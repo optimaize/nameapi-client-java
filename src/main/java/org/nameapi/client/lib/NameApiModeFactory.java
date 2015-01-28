feb3f4f722b26417a0db76cc7bbf9272b4ab6952
Fabian Kessler <fabian.kessler@optimaize.com>
2015-01-28 22:32:52 +0200

    Made it easier for the user to configure api version and host/protocol for the connection.

--- src/main/java/org/nameapi/client/lib/NameApiModeFactory.java
+++ src/main/java/org/nameapi/client/lib/NameApiModeFactory.java
@@ -4,6 +4,7 @@ import com.optimaize.command4j.Mode;
 import com.optimaize.command4j.ext.extensions.exception.exceptiontranslation.CombinedExceptionTranslator;
 import com.optimaize.command4j.ext.extensions.exception.exceptiontranslation.ExceptionTranslationExtension;
 import com.optimaize.soapworks.client.Keys;
+import com.optimaize.soapworks.client.PortUrlFactory;
 import com.optimaize.soapworks.client.exensions.exceptiontranslation.DefaultClientExceptionTranslator;
 import com.optimaize.soapworks.client.exensions.exceptiontranslation.SoapFaultExceptionTranslator;
 import com.optimaize.soapworks.common.host.Host;
@@ -18,22 +19,35 @@ import org.nameapi.ontology4.input.context.Context;
  */
 public class NameApiModeFactory {
 
-    private static final Mode minimalMode = Mode.create()
-            .with(Keys.PORT_URL_FACTORY, new NameApiPortUrlFactory())
-            .with(ExceptionTranslationExtension.TRANSLATOR, new CombinedExceptionTranslator(new DefaultClientExceptionTranslator(), new SoapFaultExceptionTranslator()))
-            .with(Keys.HOST, new Host("api.nameapi.org", 80))
-    ;
+    private static final Host DEFAULT_HOST = new Host("api.nameapi.org", 80);
+    private static final NameApiPortUrlFactory DEFAULT_PORT_FACTORY = NameApiPortUrlFactory.versionLatestStable();
+
 
     /**
      * You can take this and extend for your setup if you need more.
      *
      * <p>Example: .with(StdoutLoggingExtension.enabled())</p>
      *
-     * <p>If the minimal default is not exactly what you need, for example because you were given a different
-     * host name, then create your own from scratch.</p>
+     * @param context for example {@code new ContextBuilder().apiKey("your-api-key).priority(Priority.REALTIME).build()}
+     * @param host for example {@code new Host("api.nameapi.org", 80)}
+     * @param portUrlFactory for example {@code NameApiPortUrlFactory.versionLatestStable()}
      */
     @NotNull
-    public static Mode minimal(Context context) {
-        return minimalMode.with(NameApiKeys.CONTEXT, Conversions.convert(context));
+    public static Mode minimal(@NotNull Context context, @NotNull Host host, @NotNull PortUrlFactory portUrlFactory) {
+        return Mode.create()
+                .with(Keys.PORT_URL_FACTORY, portUrlFactory)
+                .with(ExceptionTranslationExtension.TRANSLATOR, new CombinedExceptionTranslator(new DefaultClientExceptionTranslator(), new SoapFaultExceptionTranslator()))
+                .with(Keys.HOST, host)
+                .with(NameApiKeys.CONTEXT, Conversions.convert(context));
+    }
+
+    /**
+     * Overloaded method that uses
+     * for host: {@code new Host("api.nameapi.org", 80)}
+     * for port url: {@code NameApiPortUrlFactory.versionLatestStable()}
+     */
+    @NotNull
+    public static Mode minimal(@NotNull Context context) {
+        return minimal(context, DEFAULT_HOST, DEFAULT_PORT_FACTORY);
     }
 }
--- src/main/java/org/nameapi/client/lib/NameApiPortUrlFactory.java
+++ src/main/java/org/nameapi/client/lib/NameApiPortUrlFactory.java
@@ -10,10 +10,53 @@ import java.net.URL;
  */
 public class NameApiPortUrlFactory extends AbstractPortUrlFactory {
 
+    private final String soapPrefix;
+
     /**
-     * This is updated whenever the server api version changes.
+     * This is updated whenever the "latest stable" server api version changes.
+     *
+     * Currently this is version 4.0.
      */
-    private static final String soapPrefix = "/soap/v4.0";
+    public static NameApiPortUrlFactory versionLatestStable() {
+        return version4_0();
+    }
+
+    /**
+     * This is always set to the latest release candidate, that is the version right before it becomes the latest stable.
+     * Such a version does not always exist. After successful testing, it becomes the live version.
+     *
+     * Currently this is version 4.1.
+     */
+    public static NameApiPortUrlFactory versionLatestReleaseCandidate() {
+        return version4_1();
+    }
+
+    /**
+     * This is always set to the current main development version.
+     * You do not necessarily have access to this.
+     *
+     * Currently this is version 4.2.
+     */
+    public static NameApiPortUrlFactory versionLatestDevelopment() {
+        return version4_2();
+    }
+
+    public static NameApiPortUrlFactory version4_0() {
+        return new NameApiPortUrlFactory("4.0");
+    }
+    public static NameApiPortUrlFactory version4_1() {
+        return new NameApiPortUrlFactory("4.1");
+    }
+    public static NameApiPortUrlFactory version4_2() {
+        return new NameApiPortUrlFactory("4.2");
+    }
+
+    /**
+     * @param version for example "4.0".
+     */
+    private NameApiPortUrlFactory(@NotNull String version) {
+        this.soapPrefix = "/soap/v"+version;
+    }
 
     @NotNull @Override
     public URL createUrl(@NotNull Host host, @NotNull String servicePath) {
