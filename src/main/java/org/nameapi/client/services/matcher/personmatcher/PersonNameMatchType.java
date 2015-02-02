b163ce4f20246e4d3153d914cd5b2b50bb240fd9
Fabian Kessler <fabian.kessler@optimaize.com>
2015-02-02 15:42:26 +0200

    PersonNameMatchType got an additional value in v4.1.

--- src/main/java/org/nameapi/client/services/matcher/personmatcher/PersonNameMatchType.java
+++ src/main/java/org/nameapi/client/services/matcher/personmatcher/PersonNameMatchType.java
@@ -6,8 +6,21 @@ package org.nameapi.client.services.matcher.personmatcher;
 public enum PersonNameMatchType {
 
     EQUAL,
+
     MATCHING,
+
     SIMILAR,
-    DIFFERENT;
+
+    /**
+     * @since v4.1
+     */
+    NO_SIMILARITY_FOUND,
+
+    DIFFERENT,
+    ;
+
+    public static void assertSize(int expectedItems) {
+        assert values().length == expectedItems : "Update the code calling this with " + expectedItems + "!";
+    }
 
 }
