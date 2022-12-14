0af801ea02f8bf839ed9f6d4eda5d2c9c864549d
Gabriela Achim <gabriela.achim@optimaize.com>
2022-12-14 13:39:36 +0200

    Moved the functional tests.

---
+++ src/test/java/org/nameapi/client/services/matcher/personmatcher/PersonMatcherCommandTest.java
@@ -0,0 +1,55 @@
+package org.nameapi.client.services.matcher.personmatcher;
+
+import com.optimaize.command4j.CommandExecutor;
+import com.optimaize.command4j.Mode;
+import org.nameapi.client.services.AbstractTest;
+import org.nameapi.client.services.FunctionalTestsNameApiModeFactory;
+import org.nameapi.client.lib.NameApiRemoteExecutors;
+import org.nameapi.ontology5.input.entities.person.NaturalInputPerson;
+import org.nameapi.ontology5.input.entities.person.NaturalInputPersonBuilder;
+import org.nameapi.ontology5.services.matcher.personmatcher.PersonMatchType;
+import org.nameapi.ontology5.services.matcher.personmatcher.PersonMatcherResult;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ */
+public class PersonMatcherCommandTest extends AbstractTest {
+
+    private final CommandExecutor executor = NameApiRemoteExecutors.get();
+
+    @Test
+    public void test_equal() throws Exception {
+        PersonMatcherCommand command = new PersonMatcherCommand();
+        Mode mode = FunctionalTestsNameApiModeFactory.functionalTest();
+        NaturalInputPerson person1 = new NaturalInputPersonBuilder().name(makeName("Petra Müller")).build();
+        NaturalInputPerson person2 = new NaturalInputPersonBuilder().name(makeName("Petra Müller")).build();
+        PersonMatcherArgument argument = new PersonMatcherArgument(person1, person2);
+        PersonMatcherResult result = executor.execute(command, mode, argument).get();
+        assertEquals(result.getMatchType(), PersonMatchType.EQUAL);
+    }
+
+    @Test
+    public void test_matching_1() throws Exception {
+        PersonMatcherCommand command = new PersonMatcherCommand();
+        Mode mode = FunctionalTestsNameApiModeFactory.functionalTest();
+        NaturalInputPerson person1 = new NaturalInputPersonBuilder().name(makeName("Petra Müller")).build();
+        NaturalInputPerson person2 = new NaturalInputPersonBuilder().name(makeName("Petra Mueller")).build();
+        PersonMatcherArgument argument = new PersonMatcherArgument(person1, person2);
+        PersonMatcherResult result = executor.execute(command, mode, argument).get();
+        assertEquals(result.getMatchType(), PersonMatchType.MATCHING);
+    }
+
+    @Test
+    public void test_matching_2() throws Exception {
+        PersonMatcherCommand command = new PersonMatcherCommand();
+        Mode mode = FunctionalTestsNameApiModeFactory.functionalTest();
+        NaturalInputPerson person1 = new NaturalInputPersonBuilder().name(makeName("Petra K. Müller")).build();
+        NaturalInputPerson person2 = new NaturalInputPersonBuilder().name(makeName("Petra Mueller-Meyer")).build();
+        PersonMatcherArgument argument = new PersonMatcherArgument(person1, person2);
+        PersonMatcherResult result = executor.execute(command, mode, argument).get();
+        assertEquals(result.getMatchType(), PersonMatchType.MATCHING);
+    }
+
+}
